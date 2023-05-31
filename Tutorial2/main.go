5fa84303f55124bf85c1791221e6231fd47ed06c
Thanh Nguyen <s3915468@rmit.edu.vn>
2023-06-01 00:05:26 +0700

    update tutorial 3

--- Tutorial2/main.go
+++ Tutorial2/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/rand"
 	"math/cmplx"
+	"math/rand"
 )
 
 var (
-	ToBe bool = false
-	MaxInt uint64 = 1<<64 -1
-	z complex128 = cmplx.Sqrt(-5 + 12i)
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func packages() {
@@ -29,24 +29,54 @@ func add(x, y int) int {
 	return x + y
 }
 
-func swap(x, y string)(string,string)  {
-	return y,x
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
-func split (sum int) (x,y int){
-	x = sum*4/9
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
 	y = sum - x
 	return
 }
 
-var i,j int = 1, 2
+var i, j int = 1, 2
 
-func zero()  {
+func zero() {
 	var i int
 	var f float64
 	var b bool
 	var s string
-	fmt.Printf("%v %v %v %q\n",i,f,b,s)
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
+
+func type_conversions() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+}
+
+func constants() {
+	const Pi = 3.14
+	const World = "Vietnam"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
+
+const (
+	Big   = 1 << 100
+	Small = Big >> 99
+)
+
+func needInt(x int) int {
+	return x*10 + 1
+}
+
+func needFloat(x float64) float64 {
+	return x * 0.1
 }
 
 func main() {
@@ -69,4 +99,9 @@ func main() {
 	// fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 	// fmt.Printf("Type: %T Value: %v\n", z, z)
 	// zero()
+	// type_conversions()
+	// constants()
+	// fmt.Println(needInt(Small))
+	// fmt.Println(needFloat(Small))
+	// fmt.Println(needFloat(Big))
 }
