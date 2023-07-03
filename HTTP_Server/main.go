96843c9e72d2a3eea51a81ff7f20fcf4e1d0214a
Thanh Nguyen <s3915468@rmit.edu.vn>
2023-07-03 15:16:21 +0700

    update http server hw

--- HTTP_Server/main.go
+++ HTTP_Server/main.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
@@ -11,17 +11,17 @@ import(
 	"gorm.io/gorm"
 )
 
-type Database struct{
+type Database struct {
 	connection *gorm.DB
 }
 
 var (
-	db *Database
+	db      *Database
 	dbMutex sync.Mutex
-	once sync.Once
+	once    sync.Once
 )
 
-func GetDatabase() *Database{
+func GetDatabase() *Database {
 	once.Do(func() {
 		dbMutex.Lock()
 		defer dbMutex.Unlock()
@@ -31,77 +31,92 @@ func GetDatabase() *Database{
 	return db
 }
 
-func (db *Database) initConnection(){
-	dsn :="root:root123@tcp(localhost:30001)/Students"
-	conn,err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		if err!= nil{
-			log.Fatal(err)
-		}
+func (db *Database) initConnection() {
+	dsn := "root:root123@tcp(localhost:30001)/Students"
+	conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
 	db.connection = conn
 }
 
-func (db *Database) CloseConnection(){
+func (db *Database) CloseConnection() {
 	sqlDB, err := db.connection.DB()
 	if err != nil {
-		log.Fatalf("Failed to get SQL DB: %v",err)
+		log.Fatalf("Failed to get SQL DB: %v", err)
 	}
 	sqlDB.Close()
 }
 
-type Student struct{
-	ID uint 
+type Student struct {
+	ID          uint
 	StudentName string
-	ClassID uint
-	ClassInfo Class `gorm:"foreignKey:ClassID;->"`
+	ClassID     uint
+	ClassInfo   Class `gorm:"foreignKey:ClassID;->"`
 	// ExamInfo []Exam `gorm:"foreignKey:StudentID;->"`
 }
 
-type Class struct{
-	ID uint
-	ClassName string 
+type Class struct {
+	ID        uint
+	ClassName string
 }
 
-type Subject struct{
-	ID uint 
-	SubjectName string 
+type Subject struct {
+	ID          uint
+	SubjectName string
 }
 
-type StudentSubject struct{
-	StudentID uint 
-	SubjectID uint 
+type StudentSubject struct {
+	StudentID uint
+	SubjectID uint
 }
 
 // type Exam struct{
-// 	ID uint 
+// 	ID uint
 // 	StudentID uint
-// 	Mark uint 
+// 	Mark uint
 // }
 
-func getUserHandler(c *gin.Context){
+func getUserHandler(c *gin.Context) {
 	db := GetDatabase()
 	var student []Student
 	result := db.connection.Find(&student)
-	if result.Error!= nil{
+	if result.Error != nil {
 		c.String(http.StatusInternalServerError, "Failed to fetch data from the database")
 		return
 	}
-	
+
 	// for _,s:= range student{
 	// 	fmt.Printf("%+v\n",s)
 	// }
 	var studentName []Student
 	err := db.connection.Preload("ClassInfo").Find(&studentName).Error
-	if err!= nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(studentName)
 
 	c.JSON(http.StatusOK, student)
 }
-func main(){
+
+func search(c *gin.Context) {
+	db := GetDatabase()
+	searchQuery := c.Query("name")
+
+	var students []Student
+	err := db.connection.Where("student_name LIKE ?", "%"+searchQuery+"%").Find(&students).Error
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Failed to fetch data from the database")
+		return
+	}
+	c.JSON(http.StatusOK, students)
+}
+
+func main() {
 	r := gin.Default()
 
-	r.GET("/v1/students", getUserHandler)  
+	r.GET("/v1/students", getUserHandler)
+	r.GET("/v1/students/search", search)
 	r.Run(":8080")
 	defer GetDatabase().CloseConnection()
 }
