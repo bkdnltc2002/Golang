e5dc6c6518fa44f2391920d4e22be2a1be14e318
Thanh Nguyen <s3915468@rmit.edu.vn>
2023-07-26 11:47:35 +0700

    add new method

--- channel/run_forever/main.go
+++ channel/run_forever/main.go
@@ -3,26 +3,40 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
+	// "os/signal"
+	// "syscall"
+	"bufio"
 )
 
 
 
 func main() { 
-	sigs := make(chan os.Signal, 1)
+	// method 1
+	// sigs := make(chan os.Signal, 1)
 
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	// signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	done := make(chan bool,1)
+	// done := make(chan bool,1)
 
-	go func ()  {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <-true
-	}()
-	fmt.Println("awaiting signal")
-	<-done
-	fmt.Println("exiting")
+	// go func ()  {
+	// 	sig := <-sigs
+	// 	fmt.Println()
+	// 	fmt.Println(sig)
+	// 	done <-true
+	// }()
+	// fmt.Println("awaiting signal")
+	// <-done
+	// fmt.Println("exiting")
+
+	// method 2
+	fmt.Println("Press Enter to stop")
+	reader := bufio.NewReader(os.Stdin)
+
+	_, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+
+	fmt.Println("Program stopped")
 }
