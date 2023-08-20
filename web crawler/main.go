57cfdeba34204369d11d01a0dc5a638faa625b3e
Thanh Nguyen <s3915468@rmit.edu.vn>
2023-08-20 21:32:01 +0700

    add web crawler

---
+++ web crawler/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gocolly/colly"
+)
+
+func main(){
+
+	baseURL := "tiki.vn"
+	startingURL := "https://" +baseURL
+	allowedURL := []string{baseURL}
+
+
+
+	c := colly.NewCollector(
+		colly.AllowedDomains(allowedURL...),
+		colly.Async(true),
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Limit(&colly.LimitRule{DomainGlob: "*",Parallelism: 5})
+
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Getting error",err)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Visited",r.Request.URL)
+	})
+
+	c.OnHTML("a[href]",func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	fmt.Println("Starting crawl at: ",startingURL)
+
+	if err:= c.Visit(startingURL); err!=nil{
+		fmt.Println("Error on start of crawl:",err)
+	}
+	c.Wait()
+
+}
