745456203544690ebf400bcc0399dfe7066e4d00
Thanh Nguyen <s3915468@rmit.edu.vn>
2023-08-30 15:47:14 +0700

    web crawler content, title, url

--- web crawler/main.go
+++ web crawler/main.go
@@ -3,51 +3,68 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/gocolly/colly"
 )
 
-func main(){
+type Web struct {
+	id                  int
+	title, content, url string
+}
 
-	baseURL := "tiki.vn"
-	startingURL := "https://" +baseURL
+var webinfo []Web
+
+func main() {
+	count := 0
+	baseURL := "www.bachhoaxanh.com"
+	startingURL := "https://" + baseURL
 	allowedURL := []string{baseURL}
-
-
+	web := Web{}
 
 	c := colly.NewCollector(
 		colly.AllowedDomains(allowedURL...),
 		colly.Async(true),
 	)
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
+	// c.OnRequest(func(r *colly.Request) {
+	// 	fmt.Println("Visiting", r.URL)
+	// })
 
-	c.Limit(&colly.LimitRule{DomainGlob: "*",Parallelism: 5})
+	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 1})
 
 	c.OnError(func(r *colly.Response, err error) {
-		log.Println("Getting error",err)
+		log.Println("Getting error", err)
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("Visited",r.Request.URL)
+		// fmt.Println("Visited", r.Request.URL)
+		web.url = r.Request.AbsoluteURL("")
+		// web.content = string(r.Body)
+		fmt.Println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",count)
+		count = count +1
+	})
+
+	c.OnHTML("head", func(e *colly.HTMLElement) {
+		web.title = e.ChildText("title")
+		webinfo = append(webinfo, web)
+		fmt.Println(web.url,web.title)
+		fmt.Println("bbbbbbbbbbbbbbbbbbbbbbbbb",count)
+		time.Sleep(time.Millisecond*300)
+		fmt.Println("ccccccccccccccccccccc",count)
+		fmt.Println("")
+		count = count +1
 	})
 
 	c.OnHTML("a[href]",func(e *colly.HTMLElement) {
 		e.Request.Visit(e.Attr("href"))
 	})
 
-	c.OnHTML("title",func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
-		
-	})
+	fmt.Println("Starting crawl at: ", startingURL)
 
 
-	fmt.Println("Starting crawl at: ",startingURL)
-
-	if err:= c.Visit(startingURL); err!=nil{
-		fmt.Println("Error on start of crawl:",err)
+	if err := c.Visit(startingURL); err != nil {
+		fmt.Println("Error on start of crawl:", err)
 	}
 	c.Wait()
 
