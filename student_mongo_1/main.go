d91a96a6901089b65e91b75a3c9829031986484d
Thanh Nguyen <95325717+bkdnltc2002@users.noreply.github.com>
2023-10-07 22:36:11 +0700

    using project, clean code

--- student_mongo_1/main.go
+++ student_mongo_1/main.go
@@ -11,10 +11,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type WebsiteData struct {
-	URL     string `bson:"url"`
-	Content string `bson:"content"`
-	Title   string `bson:"title"`
+type Student struct {
+	FirstName string  `bson:"first_name"`
+	LastName  string  `bson:"last_name"`
+	Grade     float64 `bson:"grade"`
 }
 
 func main() {
@@ -29,47 +29,50 @@ func main() {
 	// Select a database and collection
 	collection := client.Database("local").Collection("student")
 
-	// create group stage
-	// groupStage := bson.D{
-	// 	{"$group", bson.D{
-	// 		{"_id", "$last_name"},
-	// 		{"averageGrade", bson.D{{"$avg", "$grade"}}},
-	// 		{"Grade", bson.D{{"$push", "$grade"}}},
-	// 		{"firstNames", bson.D{{"$push", "$first_name"}}},
-	// 		{"lastNames", bson.D{{"$first", "$last_name"}}},
-	// 		{"Quantity", bson.D{{"$count", bson.D{}}}},
-	// 	}}}
+	// Define your aggregation pipeline with $group and $project stages
+	pipeline := []bson.M{
+		{
+			"$group": bson.M{
+				"_id":          "$last_name",
+				"averageGrade": bson.M{"$avg": "$grade"},
+				"Grade":        bson.M{"$push": "$grade"},
+				"firstNames":   bson.M{"$push": "$first_name"},
+				"lastNames":    bson.M{"$first": "$last_name"},
+				"Quantity":     bson.M{"$sum": 1},
+			},
+		},
+		{
+			"$project": bson.M{
+				"_id":          0,
+				"averageGrade": 1,
+				"Grade":       "$Grade",
+				"firstNames":   "$firstNames",
+				"lastNames":    "$lastNames",
+				"Quantity":     "$Quantity",
+				"is_passed": bson.M{
+					"$cond": bson.M{
+						"if":   bson.M{"$gte": []interface{}{"$averageGrade", 7}},
+						"then": "Passed",
+						"else": "Failed",
+					},
+				},
+			},
+		},
+	}
 
-	groupStage := bson.M{
-		"$group": bson.M{
-			"_id":          "$last_name",
-			"averageGrade": bson.M{"$avg": "$grade"},
-			"Grade":        bson.M{"$push": "$grade"},
-			"firstNames":   bson.M{"$push": "$first_name"},
-			"lastNames":    bson.M{"$first": "$last_name"},
-			"Quantity":     bson.M{"$count": bson.M{}},
-		}}.project({ item: 1, status: 1 });
-
-	// pass the pipeline to the Aggregate() method
-	cursor, err := collection.Aggregate(context.TODO(), bson.A{groupStage})
+	// Pass the pipeline to the Aggregate() method
+	cursor, err := collection.Aggregate(context.TODO(), pipeline)
 	if err != nil {
 		panic(err)
 	}
 
-	// display the results
+	// Display the results
 	var results []bson.M
 	if err = cursor.All(context.TODO(), &results); err != nil {
 		panic(err)
 	}
-	// for _, result := range results {
-	// 	fmt.Printf("Average Grade: %v \n", result["averageGrade"])
-	// 	fmt.Printf("Grade: %v \n", result["Grade"])
-	// 	fmt.Printf("First names: %v \n", result["firstNames"])
-	// 	fmt.Printf("Last names: %v \n", result["lastNames"])
-	// 	fmt.Printf("Quantity: %v \n\n", result["Quantity"])
-	// }
 
 	message, _ := json.MarshalIndent(results, "", " ")
-	fmt.Println(string(message))
-
+	fmt.Print(string(message))
+	fmt.Println()
 }
